f65360107eed0e2421e06cd62d115ab8707550a5
crypt <crypt@master>
2011-01-18 19:24:03 +0000

    add snippet.js

---
+++ snippet.js
@@ -0,0 +1,60 @@
+
+
+function normalize_angle(n, is_rad) {
+    if (is_rad !== undefined) {
+	var pi2 = 2 * Math.PI;
+	return (pi2 + (n % pi2)) % pi2;
+    }
+    return (360 + (n % 360)) % 360;
+}
+
+function angle_between(n, a, b) {
+    n = (360 + (n % 360)) % 360;
+    a = (360 + (a % 360)) % 360;
+    b = (360 + (b % 360)) % 360;
+
+    if (a < b)
+	return a <= n && n <= b;
+    return 0 <= n && n <= b || a <= n && n < 360;
+}
+
+Array.prototype.multipush = function () {
+    for(var i in arguments) {
+	this[this.length] = arguments[i];
+    }
+}
+
+Object.prototype.excludeLargestAmmount = function(threshold) {
+
+    if (threshold === undefined) {
+	threshold = 2;
+    }
+
+    var tmp = [];
+    var max = 0;
+    var val = 0;
+
+    for (var i in this) {
+
+	if (this[i] in tmp) {
+	    tmp[this[i]] = 1;
+	} else {
+	    tmp[this[i]]++;
+	}
+	if (tmp[this[i]] > max) {
+	    val = this[i];
+	    max = tmp[val];
+	}
+    }
+
+    if (max < threshold) {
+	return null;
+    }
+
+    for (var i in this) {
+	if (this[i] == val) {
+	    delete this[i];
+	}
+    }
+    return val;
+}
