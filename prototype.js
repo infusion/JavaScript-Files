b7d85230f4beb354fde7f605cb24a1277464ed8f
Robert Eisele <robert@xarg.org>
2014-07-30 01:01:11 +0200

    merged prototypes

--- prototype.js
+++ prototype.js
@@ -21,51 +21,90 @@ Array.prototype.sum = function() {
     return sum;
 };
 
-Math.log2 = function(x) {
+Math["asinh"] = function(x) {
+    return Math.log(x + Math.sqrt(x * x + 1));
+};
+
+Math["acosh"] = function(x) {
+    return Math.log(x + Math.sqrt(x * x - 1));
+};
+
+Math["atanh"] = function(x) {
+
+    // 0.5 * (log(1 / (1 - x)) + log(x + 1))
+
+    return 0.5 * Math.log((1 + x) / (1 - x));
+};
+
+Math["xround"] = function(x) {
+    return Math.pow(10, Math.ceil(Math["log10"](x)));
+};
+
+Math["truncate"] = function(n) {
+    return n >= 0 ? Math.floor(n) : Math.ceil(n);
+};
+
+Math["expm1"] = function(x) {
+    return (x < 1e-5 && -1e-5 < x) ? x + .5 * x * x : Math.exp(x) - 1;
+};
+
+Math["log2"] = function(x) {
     return Math.log(x) / Math.LN2;
 };
 
-Math.log10 = function(x) {
+Math["log10"] = function(x) {
     return Math.log(x) / Math.LN10;
 };
 
-Math.cosh = function(x) {
+Math["cosh"] = function(x) {
     return (Math.exp(x) + Math.exp(-x)) / 2;
 };
 
-Math.sinh = function(x) {
+Math["sinh"] = function(x) {
     return (Math.exp(x) - Math.exp(-x)) / 2;
 };
 
-Math.tanh = function(x) {
+Math["tanh"] = function(x) {
 
-    var t = Math.exp(2 * x);
-    return (t - 1) / (t + 1);
+    // sinh(2x) / (cosh(2x) + 1)
+    // (exp(a) - exp(-a)) / (exp(a) + exp(-a))
+    // (e^(2a) - 1) / (e^(2a) + 1)
+
+    return 1 - 2 / (Math.exp(2 * x) + 1);
 };
 
-String.random = function(len) {
+Math["sgn"] = function(x) {
+
+    // x < 0 ? -x : x
+    // x > 0 ? x : -x
+
+    return (0 < x) - (x < 0);
+};
+
+
+String["random"] = function(len) {
 
     len = len || 32;
 
     var chars = "abcdefghijklmnopqrstuvwxyz0123456789".split("");
 
     for (var str = ""; len--; ) {
-        str+= chars[36 * Math.random() | 0];
+        str += chars[36 * Math.random() | 0];
     }
     return str;
 };
 
-String.prototype.trim = function() {
-    return this.replace(/^\s+|\s+$/, '');
+String.prototype["trim"] = function() {
+    return this.replace(/^\s+/, '').replace(/\s+$/, '');
 };
 
-String.prototype.truncate = function(len, app) {
+String.prototype["truncate"] = function(len, app) {
 
-    if (typeof app !== "string")
+    if (app === undefined)
         app = "...";
 
     if (len < this.length - app.length) {
-        return this.substr(0, len) + app;
+        return this.slice(0, len) + app;
     }
     return String(this);
 };
@@ -83,17 +122,30 @@ String.prototype.truncateurl = function(len, app, pos) {
     return String(this);
 };
 
-String.prototype.repeat = function(num) {
-    return new Array(num + 1).join(this);
+String.prototype["repeat"] = function(num) {
+
+    var res = '', pad = this.valueOf();
+    while (num > 0) {
+        if (num & 1)
+            res += pad;
+        num >>= 1;
+        pad += pad;
+    }
+    return res;
 };
 
-String.prototype.subpart = function(map) {
+String.prototype["subpart"] = function(map) {
 
     var ret = [];
     for (var i in map) {
         ret.push(this.substr(map[i][0], map[i][1]));
     }
     return ret;
+};
+
+String.prototype["remove"] = function(f, t) {
+
+    return !t || f + t > this.length ? this.substr(0, f) : this.substr(0, f) + this.substr(f + t);
 };
 
 Array.prototype.max = function() {
@@ -105,7 +157,8 @@ Array.prototype.min = function() {
 };
 
 Array.prototype.sum = function() {
-    for (var i = this.length, sum = 0; i--; sum += this[i]) {}
+    for (var i = this.length, sum = 0; i--; sum += this[i]) {
+    }
     return sum;
 };
 
